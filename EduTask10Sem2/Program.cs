432b9a39a5330990ce4ab5e78e21b6ce6a27aa53
Andrew_A <chen_60@mail.ru>
2022-10-06 11:31:23 +0300

    Creane and Add EduTask10Sem2

---
+++ EduTask10Sem2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Выбор второй цифры трёхзначнго числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите, пожалуйста трехзначное число. ");
+string InNumA = Console.ReadLine() ?? "";
+int numA = int.Parse(InNumA);
+
+int a1 = 0, a2 = 0;
+
+if (numA < 999)
+{
+    if (numA > 99)
+    {
+        a1 = numA % 100;
+        a2 = a1 / 10;
+        Console.WriteLine(a2);
+    }
+    else
+    {
+        Console.WriteLine("Вы нарушили условия задачи: ввели двузначное число. Введите, пожалуйста трехзначное число.");
+    }
+}
+else
+{
+    Console.WriteLine("Вы превысили разрядность. Введите, пожалуйста трехзначное число.");
+}
+Console.WriteLine();
